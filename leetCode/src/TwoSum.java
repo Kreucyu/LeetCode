8c2d224dc98c1ae1ddb00352777d4b61fb35bed5
Kreucyu <elciocleiton72@gmail.com>
2026-04-09 14:11:00 -0300

    build: TwoSum method

--- leetCode/src/TwoSum.java
+++ leetCode/src/TwoSum.java
@@ -1,16 +1,17 @@
 public class TwoSum {
         public int[] twoSum(int[] nums, int target) {
-            int sum[] = new int[2];
-            for(int i = 0; i < nums.length - 1; i++) {
-                int value = nums[i];
-                for (int j = 1; j < nums.length; j++) {
-                    if(value + nums[j] != target) continue;
+        int sum[] = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            int value = nums[i];
+            for (int j = 1; j < nums.length; j++) {
+                if(value + nums[j] == target && j != i) {
                     sum[0] = i;
                     sum[1] = j;
                     break;
                 }
             }
-            return sum;
         }
+        return sum;
+    }
     }
 
