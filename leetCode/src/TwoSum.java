f0f92a92b79e6fb7f8dacf658d220a81f64e396d
Kreucyu <elciocleiton72@gmail.com>
2026-04-09 14:25:39 -0300

    build: switched Array to HashMap for more efficient search time

--- leetCode/src/TwoSum.java
+++ leetCode/src/TwoSum.java
@@ -1,17 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
         public int[] twoSum(int[] nums, int target) {
-        int sum[] = new int[2];
-        for(int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-            for (int j = 1; j < nums.length; j++) {
-                if(value + nums[j] == target && j != i) {
-                    sum[0] = i;
-                    sum[1] = j;
-                    break;
-                }
+            Map<Integer, Integer> map = new HashMap<>();
+            int[] sum = new int[2];
+            for(int i = 1; i < nums.length; i++) {
+                map.put(nums[i], i);
             }
+            for(int i = 0; i < nums.length; i++) {
+                int value = target - nums[i];
+                if(map.containsKey(value) && i != map.get(value)) {
+                    sum[0] = i;
+                    sum[1] = map.get(value);
+                    return sum;
+                }
         }
-        return sum;
+        return null;
     }
-    }
-
+}
