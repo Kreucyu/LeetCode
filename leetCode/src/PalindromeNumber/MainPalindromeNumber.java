5e2144a3a2922d0620930d9bac266962d1f12b99
Kreucyu <elciocleiton72@gmail.com>
2026-04-15 20:49:35 -0300

    feat: Palindrome Number completed

--- leetCode/src/PalindromeNumber/MainPalindromeNumber.java
+++ leetCode/src/PalindromeNumber/MainPalindromeNumber.java
@@ -1,4 +1,8 @@
 package PalindromeNumber;
 
 public class MainPalindromeNumber {
+    public static void main(String[] args) {
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        System.out.println(palindromeNumber.isPalindrome(121));
+    }
 }
--- leetCode/src/PalindromeNumber/PalindromeNumber.java
+++ leetCode/src/PalindromeNumber/PalindromeNumber.java
@@ -3,8 +3,15 @@ package PalindromeNumber;
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         if(x < 11) return false;
-        String y = String.valueOf(x);
-        System.out.println(y.chars());
-        return false;
+        String number = String.valueOf(x);
+        Character[] numbers = new Character[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            numbers[i] = number.charAt(i);
+        }
+        StringBuilder inversedNumber = new StringBuilder();
+        for(int i = number.length() - 1; i >= 0; i--) {
+            inversedNumber.append(numbers[i]);
+        }
+        return number.contentEquals(inversedNumber);
     }
 }
